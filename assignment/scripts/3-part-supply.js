a2520450d7d6e66cdca5459240c3bdfad4973ca0
Dane Smith <doctorhowser@gmail.com>
2020-11-09 19:29:15 -0600

    Update 3-part-supply.js

--- assignment/scripts/3-part-supply.js
+++ assignment/scripts/3-part-supply.js
@@ -9,7 +9,7 @@ console.log('1. Number of partsNeeded:');
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
 
-// 3. Console log the value of 'partsUsed' at index 2
+// 3. Console log the value of 'supplyChanges' at index 2
 console.log('3. Item at index 2 is:');
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
